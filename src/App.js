3b8a60fa2972d9f4c09a93ee71fa5f5c56cadbfb
elitajailoobekova <jailoobekovaelita14@gmail.com>
2022-11-24 13:03:13 +0600

    expense-trucker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+const App =() => {
+  return (
+    <div className="App">
+  <ExpenseItem/>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenseltem.jsx
@@ -0,0 +1,12 @@
+
+const ExpenseItem = () => {
+  return ( 
+    <div> 
+     <div>Car Insuranse</div>
+     <div>20 May 2022</div>
+     <div>$200</div>
+  </div>
+  );
+};
+
+export default ExpenseItem
